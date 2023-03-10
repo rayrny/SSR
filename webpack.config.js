b7a157a8e8abf9849492894adbec3d3b4eb35ae0
Suji Kim <bion239@naver.com>
2023-03-11 01:27:34 +0900

    feat: webpack config에 ts loader 추가 및 resolve.extentions 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,24 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/server/index.ts",
+  mode: "development",
+  entry: path.resolve(__dirname, "src/server/index.ts"),
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+    alias: {
+      src: path.resolve(__dirname, "./src/"),
+      server: path.resolve(__dirname, "./src/server"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
