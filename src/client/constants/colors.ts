2b41f31e9c31f09c4828c11bea0ff3a9c8ffe4b2
Suji Kim <bion239@naver.com>
2023-04-17 23:30:34 +0900

    style: style 파일 업데이트

--- src/client/constants/colors.ts
+++ src/client/constants/colors.ts
@@ -2,13 +2,16 @@ type TColors = Record<string, string>;
 
 const colors: TColors = {
   primary: "#5b2ee0",
-  secondary: "#8c6ce9",
+  secondary: "#5CB8E4",
   success: "#1acf8b",
   danger: "#ec3e68",
   warning: "#fd7f4e",
   info: "#cfd2e1",
   white: "#f7f7f7",
   black: "#181818",
+
+  dark: "#252525",
+  light: "#F2F2F2",
 };
 
 export default colors;
--- src/client/global-style.ts
+++ src/client/global-style.ts
@@ -1,10 +1,28 @@
 import { createGlobalStyle } from "styled-components";
+import COLORS from "./constants/colors";
 
 export const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: "UhBeenamsoyoung";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_five@.2.0/UhBeenamsoyoung.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'UhBeeYeonie';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_five@.2.0/UhBeeYeonie.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+  }
+
 body {
-    margin: 0;
+    margin: 1rem 3rem;
     text-align: center;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    background-color: ${COLORS.dark};
+    color: ${COLORS.light};
+    font-family: UhBeeYeonie, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
     -webkit-font-smoothing: antialiased;
