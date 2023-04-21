186aaac3ef155c1e91fd6985546cd1d992517294
Suji Kim <bion239@naver.com>
2023-04-21 19:34:52 +0900

    fix: renderToPipeableStream 시 스타일 적용 안되던 문제 수정

--- src/server/render.tsx
+++ src/server/render.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import fs from "fs";
-import path from "path";
+import React, { ReactNode } from "react";
 import { Request, Response } from "express";
-import { renderToPipeableStream } from "react-dom/server";
+import { renderToPipeableStream, renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import { ServerStyleSheet } from "styled-components";
 
@@ -10,52 +8,36 @@ import App from "../client/App";
 import { GlobalStyle } from "../client/global-style";
 
 const styleSheet = new ServerStyleSheet();
-const template = fs.readFileSync(
-  path.resolve(__dirname, "../../public/index.html"),
-  "utf8"
-);
 
-// const render = (url: string): { html: string; styleTags: string } => {
-//   const html: string = ReactDOMServer.renderToString(
-//     styleSheet.collectStyles(
-//       <>
-//         <GlobalStyle />
-//         <StaticRouter location={url}>
-//           <App />
-//         </StaticRouter>
-//       </>
-//     )
-//   );
+const getStyleTags = (element: ReactNode) => {
+  renderToString(styleSheet.collectStyles(element));
 
-//   const styleTags = styleSheet.getStyleTags();
-
-//   return { html, styleTags };
-// };
+  return styleSheet.getStyleTags();
+};
 
 const sendStringifiedHtml = (req: Request, res: Response) => {
-  // const { html, styleTags } = render(req.url);
+  const RootApp = () => (
+    <>
+      <GlobalStyle />
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>
+    </>
+  );
 
-  // const result = template
-  //   .replace('<div id="root"></div>', `<div id="root">${html}</div>`)
-  //   .replace('<style id="server-style"></style>', styleTags);
-  // res.send(result);
-  console.log(GlobalStyle);
   const { pipe } = renderToPipeableStream(
-    styleSheet.collectStyles(
-      <>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/public/favicon.ico" />
-        <title>나는 고양이 있어!</title>
-        <style id="server-style">
-          <GlobalStyle />
-        </style>
-        <div id="root">
-          <StaticRouter location={req.url}>
-            <App />
-          </StaticRouter>
-        </div>
-      </>
-    ),
+    <>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/public/favicon.ico" />
+      <title>나는 고양이 있어!</title>
+      <style
+        id="server-style"
+        dangerouslySetInnerHTML={{ __html: getStyleTags(<RootApp />) }}
+      ></style>
+      <div id="root">
+        <RootApp />
+      </div>
+    </>,
     {
       bootstrapScripts: ["/public/dist/client/bundle.js"],
       onShellReady() {
